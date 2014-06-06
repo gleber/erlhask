bcacd46503a52f91a8a4313ba303a444fdfdd6d0
Gleb Peregud <gleber.p@gmail.com>
2014-06-06 20:35:56 +0200

    Finish implementing first proper test for ErlHask

--- testsuite/Tests.hs
+++ testsuite/Tests.hs
@@ -16,10 +16,13 @@ import Language.Erlang.Eval
 import Language.Erlang.Core
 import qualified Language.CoreErlang.Parser as P
 import Language.Erlang.Lang
-import Control.Exception (throw, SomeException, catch)
+import Control.Exception (throw, SomeException)
 import Control.Monad.State (liftIO)
 
--- THESE ARE DUMMY TESTS TO BE REPLACED WITH SOME REAL ErlHask TESTS
+import Control.Distributed.Process
+import Control.Distributed.Process.Closure
+import Control.Distributed.Process.Node
+import Network.Transport.TCP
 
 
 main :: IO ()
@@ -29,50 +32,50 @@ main = defaultMainWithOpts
        , testProperty "listRevRevId" propListRevRevId
        ] mempty
 
-lambdaTestCode = [r|module 'boot' ['lambda_test'/0]
+lambdaTestCode = [r|
+module 'lambda_test' ['lambda_test'/0]
     attributes []
-'lambda'/1 =
-    fun (_cor0) ->
-        ( fun (_cor3) ->
-              let <_cor2> = call 'erlang':'+' (_cor0, 10)
-              in call 'erlang':'+' (_cor2, _cor3)
-          -| [{'id',{0,0,'-lambda/1-fun-0-'}}] )
 'lambda_test'/0 =
     fun () ->
-        let <L> = apply 'lambda'/1 (1)
-        in apply L (5)
+        let <L> =
+            ( fun (_cor1) ->
+                  call 'erlang':'+'
+                      (1, _cor1)
+              -| [{'id',{0,0,'-lambda_test/0-fun-0-'}}] )
+        in
+            apply L
+                (5)
+end
 |]
 
 lambdaTest :: Assertion
-lambdaTest = undefined
+lambdaTest = testInProc lambdaTestCode "lambda_test" (ErlNum 6)
 
-testInProc :: String -> String -> Assertion
-testInProc modsrc fn = do
-
-  tr <- createTransport "localhost" "8981" defaultTCPParameters
-  case tr of
-    Left e -> do
-      putStrLn "Faield to create a transport"
-      print e
-    Right transport -> do
-      putStrLn "Transport created."
-      node <- newLocalNode transport initRemoteTable
-      putStrLn "Node created."
-      putStrLn "Running boot process..."
-      runProcess node bootProc
-
+testProc :: String -> String -> MVar ErlTerm -> Process ()
+testProc modsrc fn res = do
   let Right m = P.parseModule modsrc
       m' = EModule "tested" (unann m)
       mt = M.insert "tested" m' newBaseModTable
-  mmt <- newMVar mt
+  mmt <- liftIO $ newMVar mt
   let runner = applyMFA "tested" fn []
   let ev = do
-        result <- runErlProcess runner bootModule mmt newProcDict
+        Right term <- runErlProcess runner bootModule mmt newProcDict
+        liftIO $ putMVar res term
         return ()
   catch ev (\(e :: SomeException) -> do
                  liftIO $ print (show e)
                  throw e)
 
+testInProc :: String -> String -> ErlTerm -> Assertion
+testInProc modsrc fn expected = do
+  Right tr <- createTransport "localhost" "8999" defaultTCPParameters
+  node <- newLocalNode tr initRemoteTable
+  res <- newEmptyMVar
+  runProcess node (testProc modsrc fn res)
+  result <- takeMVar res
+  result @?= expected
+
+-- TESTS BELOW ARE DUMMY TESTS TO BE REPLACED WITH SOME REAL ErlHask TESTS
 testRev :: Assertion
 testRev = reverse [1, 2, 3] @?= [3, 2, 1]
 
